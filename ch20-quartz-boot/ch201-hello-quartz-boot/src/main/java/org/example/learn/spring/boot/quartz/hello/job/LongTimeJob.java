225946545e289e9bddcf64820e971f4eee08e520
maskedmarker <changjianghui0822@gmail.com>
2025-02-21 00:02:22 +0800

    add

--- ch20-quartz-boot/ch201-hello-quartz-boot/src/main/java/org/example/learn/spring/boot/quartz/hello/job/LongTimeJob.java
+++ ch20-quartz-boot/ch201-hello-quartz-boot/src/main/java/org/example/learn/spring/boot/quartz/hello/job/LongTimeJob.java
@@ -2,7 +2,6 @@ package org.example.learn.spring.boot.quartz.hello.job;
 
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,13 +10,13 @@ import java.util.concurrent.TimeUnit;
 
 public class LongTimeJob implements Job {
 
-    private static Logger logger = LoggerFactory.getLogger(LongTimeJob.class);
+    private static final Logger logger = LoggerFactory.getLogger(LongTimeJob.class);
 
     @Override
-    public void execute(JobExecutionContext context) throws JobExecutionException {
+    public void execute(JobExecutionContext context) {
         logger.info("{} is working at {}", LongTimeJob.class.getSimpleName(), new Date());
         try {
-            Thread.sleep(TimeUnit.SECONDS.toMillis(1000));
+            Thread.sleep(TimeUnit.SECONDS.toMillis(9));
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
--- ch20-quartz-boot/ch201-hello-quartz-boot/src/main/java/org/example/learn/spring/boot/quartz/hello/job/ShortTimeJob.java
+++ ch20-quartz-boot/ch201-hello-quartz-boot/src/main/java/org/example/learn/spring/boot/quartz/hello/job/ShortTimeJob.java
@@ -2,7 +2,6 @@ package org.example.learn.spring.boot.quartz.hello.job;
 
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,13 +10,13 @@ import java.util.concurrent.TimeUnit;
 
 public class ShortTimeJob implements Job {
 
-    private static Logger logger = LoggerFactory.getLogger(ShortTimeJob.class);
+    private static final Logger logger = LoggerFactory.getLogger(ShortTimeJob.class);
 
     @Override
-    public void execute(JobExecutionContext context) throws JobExecutionException {
+    public void execute(JobExecutionContext context) {
         logger.info("{} is working at {}", ShortTimeJob.class.getSimpleName(), new Date());
         try {
-            Thread.sleep(TimeUnit.SECONDS.toMillis(1000));
+            Thread.sleep(TimeUnit.SECONDS.toMillis(7));
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
